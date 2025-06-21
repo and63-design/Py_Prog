067c647879311f2f305336eac3b519870b6930ad
and63-design <aspr63@pm.me>
2025-06-21 09:25:52 +0100

    new files

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print('Hello World!')
-
-if True:
-    pass
-
-while True:
-    pass
+def do_stuff(num=0):
+    try:
+        if num:
+            return int(num) + 5
+        else:
+            return 'Please enter a number'
+    except ValueError as err:
+        return err
